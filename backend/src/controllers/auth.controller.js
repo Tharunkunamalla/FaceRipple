47ed04f4dd0f270b9938bf6649b585abd386630e
Tharunkunamalla <kunamallatharun701@gmail.com>
2025-05-13 00:30:21 +0530

    Steam and mongo connections done

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export async function signup(req, res) {
+  // Handle signup logic here
+  res.send("Signup Route");
+}
+export async function login(req, res) {
+  // Handle login logic here
+  res.send("Login Route");
+}
+export function logout(req, res) {
+  // Handle logout logic here
+  res.send("Logout Route");
+}
