d5d46f61e26277cb0bcc94e61bfe8268f05f9683
Tharunkunamalla <kunamallatharun701@gmail.com>
2025-05-17 01:03:08 +0530

    Backend Completed.

---
+++ backend/src/controllers/chat.controller.js
@@ -0,0 +1,16 @@
+import {generateStreamToken} from "../lib/stream.js";
+
+export async function getStreamToken(req, res) {
+  try {
+    const token = await generateStreamToken(req.user.id);
+    res.status(200).json({
+      success: true,
+      token,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import {connectDB} from "./lib/db.js";
+import chatRoutes from "./routes/chat.route.js";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -14,6 +15,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
