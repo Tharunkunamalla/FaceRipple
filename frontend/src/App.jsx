63289999cff983c8bffc3a86b08978f700e9033e
Tharunkunamalla <kunamallatharun701@gmail.com>
2025-05-19 02:12:05 +0530

    Added React-Toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,27 @@ import ChatPage from "./pages/ChatPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import CallPage from "./pages/CallPage.jsx";
 
+import toast, {Toaster} from "react-hot-toast";
+
 const App = () => {
   return (
-    
+    <div className="h-screen text-white " data-theme="dark">
+      <button onClick={() => toast.success("Toast Added")}>
+        create a toast
+      </button>
+      <br />
+      <button onClick={() => toast.error("This is an error")}> Error</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+      </Routes>
+      <Toaster />
+    </div>
   );
 };
 
