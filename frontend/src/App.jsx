238669a72b435d3036e62d9395ad4e3ecb72a08f
Tharunkunamalla <kunamallatharun701@gmail.com>
2025-05-20 15:51:04 +0530

    added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
         <Route path="/chat" element={<ChatPage />} />
         <Route path="/onboarding" element={<OnboardingPage />} />
       </Routes>
-      <Toaster />
+      <Toaster /> // This will render the toast notifications
     </div>
   );
 };
