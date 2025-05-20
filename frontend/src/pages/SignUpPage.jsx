ef296c2fb564c37141ad8bdff0216acd2f6f4715
Tharunkunamalla <kunamallatharun701@gmail.com>
2025-05-21 01:36:17 +0530

    signup

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 const SignupPage = () => {
-  return <div>Hiii</div>;
+  const [signupData, setSignupData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  const handelSignup = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div
+      className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8"
+      data-theme="forest"
+    >
+      <div className="border border—primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+        {/* Sign up form left side */}
+        <div className="w-full lg:w-1/2 p-4  sm:p-8 flex flex-col">
+          {/* LOGO */}
+          <div className="mb-4 flex items-center justify-start gap-2"></div>
+        </div>
+      </div>
+    </div>
+  );
+  return <div></div>;
 };
 
 export default SignupPage;
