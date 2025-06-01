561cc80aa2eae93372911d58169aaec5b946d795
Tharunkunamalla <kunamallatharun701@gmail.com>
2025-06-01 11:42:58 +0530

    Onboarding

--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import React, {useState} from "react";
+import toast from "react-hot-toast";
 
 const OnboardingPage = () => {
   const {isLoading, authUser} = useAuthUser();
+  const queryClient = useQueryClient();
+  const {formState, setFormState} = useState({
+    fullName: authUser?.fullName || "",
+    bio: authUser?.bio || "",
+    nativeLanguage: authUser?.nativeLanguage || "",
+    learningLanguage: authUser?.learningLanguage || "",
+    location: authUser?.location || "",
+    profilePic: authUser?.profilePic || "",
+  });
+
+  const {mutate: onboardingMutation, isPending} = useMutation({
+    mutationFn: completeOnboarding,
+    onSuccess: () => {
+      toast.success("Profile Onboarding successfully");
+      queryClient.invalidateQueries({queryKey: ["authUser"]});
+    },
+  });
 
   return <div></div>;
 };
